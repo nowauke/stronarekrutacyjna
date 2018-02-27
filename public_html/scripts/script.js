d9f5bca88d48c9309efe361121d3f5667dbd2ffb
NOWAUKE <NOWAUKE@NOWAUKE-KOMPUTE>
2018-02-27 20:29:34 +0100

    strona iskrzy 2

--- public_html/scripts/script.js
+++ public_html/scripts/script.js
@@ -36,5 +36,13 @@ $(function () {
     }
 
 
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 });
 
